09119452bba6d25bb0d299acc964b49aa520c4fd
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-12-22 01:00:06 -0300

    Performance de consultas: Consultas com Join Fetch

--- src/main/java/br/com/alura/loja/dao/PedidoDao.java
+++ src/main/java/br/com/alura/loja/dao/PedidoDao.java
@@ -49,4 +49,14 @@ public class PedidoDao {
 				.getResultList();
 	}
 
+	public Pedido buscarPedidoCliente(Long id) {
+		String jpql = "SELECT p FROM Pedido p " + 
+				"JOIN FETCH p.cliente " + 
+				"WHERE p.id = :id";
+		return entityManager
+				.createQuery(jpql, Pedido.class)
+				.setParameter("id", id)
+				.getSingleResult();
+	}
+
 }
--- src/main/java/br/com/alura/loja/teste/PerformanceConsultas.java
+++ src/main/java/br/com/alura/loja/teste/PerformanceConsultas.java
@@ -2,6 +2,7 @@ package br.com.alura.loja.teste;
 
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.PedidoDao;
 import br.com.alura.loja.modelo.Pedido;
 import br.com.alura.loja.util.JPAUtil;
 
@@ -13,13 +14,24 @@ public class PerformanceConsultas {
 
 		EntityManager entityManager = JPAUtil.getEntityManager();
 
-		Pedido pedido = entityManager.find(Pedido.class, 1l);
+		PedidoDao pedidoDao = new PedidoDao(entityManager);
+
+		Pedido pedido = pedidoDao.buscarPedidoCliente(1l);
 
 		// Relacionamentos ToOne são carregados por padrão na JPA (Eager)
 		System.out.println("Data pedido: " + pedido.getData());
 
 		// Relacionamentos ToMany são consultados/carregados apenas quando acessados (Lazy)
 		System.out.println("Quantidade de itens: " + pedido.getItensPedidos().size());
+
+		entityManager.close();
+
+		// Executa normalmente após fechamento do EntityManager
+		System.out.println("Data pedido: " + pedido.getData());
+
+		// LazyInitializationException, pois Cliente (agora definido como Lazy) não foi carregado
+		// antes do fechamento do EntityManager. A solução é definir a query e usar JOIN FETCH
+		System.out.println("Cliente: " + pedido.getCliente().getNome());
 
 	}
 
